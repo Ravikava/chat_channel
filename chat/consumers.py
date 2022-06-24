b3585c05924eeff8d2022c7c7e035f00d3d28822
Ravi Kava <kavaravi2@gmail.com>
2022-06-24 16:22:31 +0530

    changes on templates and some files add

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,53 @@
 # chat/consumers.py
+from email import message
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import *
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ChatConsumer(WebsocketConsumer):
+    
+    def fetch_messages(self, data):
+        messages = Message.last_10_messages()
+        content = {
+            'messages' : self.messages_to_json(messages)
+        }
+        self.send_message(content)
+    
+    def new_mesasge(self, data):
+        author = data['from']
+        author_user = User.objects.filter(username=author)[0]
+        message = Message.objects.create(
+            author = author_user,
+            content = data['message']
+        )
+        content = {
+            'command' : 'new_message',
+            "message": self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+    
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+    
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content':message.content,
+            'timestamp':str(message.timestamp)
+        }
+    
+    commands = {
+        'fetch_messages' : fetch_messages,
+        'new_message' : new_mesasge
+    }
+    
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
@@ -25,10 +69,10 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
+        data = json.loads(text_data)
+        self.commands[data['command']](self,data)
+        
+    def send_chat_message(self, message):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -36,12 +80,12 @@ class ChatConsumer(WebsocketConsumer):
                 'message': message
             }
         )
+        
+    def send_message(self,message):
+        self.send(text_data=json.dumps(message))
 
-    # Receive message from room group
     def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        async_to_sync(self.send(text_data=json.dumps({
-            'message': message
-        })))
+        self.send(text_data=json.dumps(message))
